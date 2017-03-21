2c5d5e6f41e57ddd0664e17d5ec434fb03bbf0a0
anujnag <anujnagpal96@gmail.com>
2017-03-21 22:41:53 +0530

    Symbol Table CSV Dump Added

--- bin/milestone2.py
+++ bin/milestone2.py
@@ -6,10 +6,22 @@ import parser
 import ply.lex as lex
 import ply.yacc as yacc
 import node_file
+import csv
+from PIL import Image
+
 parse = parser.Parser()
 tree = parse.parse_file(file('./test/test1.java'))
 parser.printing()
-print("AST printed on AST.png")
-from PIL import Image
+print("\nAST in txt format:")
+print(tree)
+print("\nSymbol Table:")
+print(parser.ST.SymbolTable)
 img = Image.open('AST.png')
 img.show()
+print("\nSymbol Table Dump Written on ST.csv")
+with open('ST.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["function","variable","modifiers","type","dimension"])
+    for a in parser.ST.SymbolTableFunction:
+         for b in parser.ST.SymbolTableFunction[a]['variables']:
+                 writer.writerow([a , b, parser.ST.SymbolTableFunction[a]['variables'][b]['modifiers'],parser.ST.SymbolTableFunction[a]['variables'][b]['type'],parser.ST.SymbolTableFunction[a]['variables'][b]['dimension']])
