5f12d39a76a7318c837283ce1e50b24edb0cc36e
Abhinav Garg <abhigarg@iitk.ac.in>
2017-04-16 22:55:09 +0530

    Completed Milestone 3
    Signed-off-by: Abhinav Garg <abhigarg@iitk.ac.in>

--- test/test1.java
+++ test/test1.java
@@ -1,12 +1,17 @@
 // For Loops to Calculate Factorial
 public class Factorial
 {
+
         static private char main(int a)
         {
             int A[][] = new int[3][5];
             int b[][][] = new int[2][3][4];
             A[b[1][2][3]][4] = 2;
-	    
+            {
+              int a;
+              a = 5;
+            }
+
 	    for(a=0;a<3;a++)
             {
                b = b + 1;
